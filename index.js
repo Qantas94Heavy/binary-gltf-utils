c724fd977415ca68f937cafec9033e06adad4d7b
Karl Cheng <qantas94heavy@gmail.com>
2016-01-09 02:30:21 +0800

    Update to 1.1.0, add embedding shaders support

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const embed = {};
 const argv = require('yargs')
   .usage('Usage: $0 <file> [options]')
   .demand(1)
-  .array('embed')
+  .array('e')
   .describe('e', 'embeds textures or shaders into binary GLTF file')
   .choices('e', embedArr)
   .alias('e', 'embed')
@@ -34,12 +34,38 @@ if (argv.embed) {
 }
 
 const filename = argv._[0];
+const BUFFER_NAME = argv.cesium ? 'KHR_binary_glTF' : 'binary_glTF';
 
 if (!filename.endsWith('.gltf')) {
   console.error('Failed to create binary GLTF file:');
   console.error('----------------------------------');
   console.error('File specified does not have the .gltf extension.');
   return;
+}
+
+// Lets us keep track of how large the body will be, as well as the offset for each of the
+// original buffers.
+let bodyLength = 0;
+const bodyParts = [];
+
+const base64Regexp = /^data:.*?;base64,/;
+const containingFolder = path.dirname(filename);
+
+function addToBody(uri) {
+  let promise;
+  if (uri.startsWith('data:')) {
+    if (!base64Regexp.test(uri)) throw new Error('unsupported data URI');
+    promise = Promise.resolve(new Buffer(uri.replace(base64Regexp, ''), 'base64'));
+  }
+  else promise = fs.readFileAsync(path.join(containingFolder, uri));
+
+  return promise.then(function (contents) {
+    const offset = bodyLength;
+    bodyParts.push(offset, contents);
+    const length = contents.length;
+    bodyLength += length;
+    return { offset, length };
+  });
 }
 
 fs.readFileAsync(filename, 'utf-8').then(function (gltf) {
@@ -50,50 +76,106 @@ fs.readFileAsync(filename, 'utf-8').then(function (gltf) {
   if (Array.isArray(scene.extensionsUsed)) scene.extensionsUsed.push('KHR_binary_glTF');
   else scene.extensionsUsed = [ 'KHR_binary_glTF' ];
 
-  // All buffer views will later reference the implicit "binary_glTF" body buffer, so it will not
-  // be needed.  We keep a reference to these old buffer definitions to create the body.
-  const buffers = scene.buffers;
-
-  // We can now remove the existing buffer definitions from the generated scene content.
-  scene.buffers = undefined;
-
-  // Lets us keep track of how large the body will be, as well as the offset for each of the
-  // original buffers.
-  let bodyLength = 0;
-
-  Object.keys(buffers).forEach(function (bufferId) {
-    const bufferInfo = buffers[bufferId];
+  const bufferPromises = [];
+  Object.keys(scene.buffers).forEach(function (bufferId) {
+    const buffer = scene.buffers[bufferId];
 
     // We don't know how to deal with other types of buffers yet.
-    const type = bufferInfo.type;
+    const type = buffer.type;
 
     if (type && type !== 'arraybuffer') {
       throw new Error(util.format('buffer type "%s" not supported: %s', type, bufferId));
     }
 
-    // Set the buffer value to the offset temporarily for easier manipulation of bufferViews.
-    bufferInfo.byteOffset = bodyLength;
+    const promise = addToBody(buffer.uri).then(function (obj) {
+      // Set the buffer value to the offset temporarily for easier manipulation of bufferViews.
+      buffer.byteOffset = obj.offset;
+    });
 
-    // Add the length of this buffer to how long body will be.
-    bodyLength += bufferInfo.byteLength;
+    bufferPromises.push(promise);
   });
 
+  // Run this on the existing buffers first so that the buffer view code can read from it.
+  return Promise.all(bufferPromises).return(scene);
+}).then(function (scene) {
   Object.keys(scene.bufferViews).forEach(function (bufferViewId) {
     const bufferView = scene.bufferViews[bufferViewId];
     const bufferId = bufferView.buffer;
-    const referencedBuffer = buffers[bufferId];
+    const referencedBuffer = scene.buffers[bufferId];
 
     if (!referencedBuffer) {
       throw new Error(util.format('buffer ID reference not found: %s', bufferId));
     }
 
-    bufferView.buffer = argv.cesium ? 'KHR_binary_glTF' : 'binary_glTF';
+    bufferView.buffer = BUFFER_NAME;
     bufferView.byteOffset += referencedBuffer.byteOffset;
   });
 
-  // TODO: embed shaders and images into body (especially if already embedded as base64)
+  const promises = [];
+  if (embed.shaders) Object.keys(scene.shaders).forEach(function (shaderId) {
+    const shader = scene.shaders[shaderId];
+    const uri = shader.uri;
+    shader.uri = '';
+
+    const promise = addToBody(uri).then(function (obj) {
+      const bufferViewId = 'binary_shader_' + shaderId;
+      shader.extensions = { KHR_binary_glTF: { bufferView: bufferViewId } };
+
+      scene.bufferViews[bufferViewId] =
+        { buffer: BUFFER_NAME
+        , byteLength: obj.length
+        , byteOffset: obj.offset
+        };
+    });
+
+    promises.push(promise);
+  });
+
+  // TODO: embed images into body (especially if already embedded as base64)
+  /*
+  Object.keys(scene.images).forEach(function (imageId) {
+    const image = scene.images[imageId];
+    const uri = image.uri;
+
+    const promise = addToBody(uri).then(function (obj) {
+      const bufferViewId = 'binary_images_' + imageId;
+      // TODO: add extension properties
+      image.extensions =
+        { KHR_binary_glTF:
+          { bufferView: bufferViewId
+          , mimeType: 'image/i-dont-know'
+          , height: 9999
+          , width: 9999
+          }
+        };
+
+      scene.bufferViews[bufferViewId] =
+        { buffer: BUFFER_NAME
+        , byteLength: obj.length
+        , byteOffset: obj.offset
+        };
+    });
+
+    promises.push(promise);
+  });
+  */
+
+  return Promise.all(promises).return(scene);
+}).then(function (scene) {
+  // All buffer views now reference the implicit "binary_glTF" buffer, so it is no longer needed.
+  if (argv.cesium) {
+    // Cesium seems to run into issues if this is not defined, even though it shouldn't be needed.
+    scene.buffers =
+      { KHR_binary_glTF:
+        { uri: ''
+        , byteLength: bodyLength
+        }
+      };
+  }
+  else scene.buffers = undefined;
 
   const newSceneStr = JSON.stringify(scene);
+  fs.writeFileSync('test', newSceneStr);
   const sceneLength = Buffer.byteLength(newSceneStr);
   // As body is 4-byte aligned, the scene length must be padded to have a multiple of 4.
   // jshint bitwise:false
@@ -130,30 +212,13 @@ fs.readFileAsync(filename, 'utf-8').then(function (gltf) {
   for (let i = sceneLength + 20; i < bodyOffset; ++i) glbFile[i] = 0x20;
 
   // Write the body.
-  const bodyPartPromises = [];
-  const containingFolder = path.dirname(filename);
+  for (let i = 0; i < bodyParts.length; i += 2) {
+    const offset = bodyParts[i];
+    const contents = bodyParts[i + 1];
+    contents.copy(glbFile, bodyOffset + offset);
+  }
 
-  Object.keys(buffers).forEach(function (bufferId) {
-    const bufferInfo = buffers[bufferId];
-
-    return Promise.try(function () {
-      const uri = bufferInfo.uri;
-
-      if (uri.startsWith('data:')) {
-        const base64Regexp = /^data:.*?;base64,/;
-        if (!base64Regexp.test(uri)) throw new Error('unsupported data URI');
-        return new Buffer(uri.replace(base64Regexp, ''), 'base64');
-      }
-
-      return fs.readFileAsync(path.join(containingFolder, uri));
-    }).then(function (buffer) {
-      buffer.copy(glbFile, bodyOffset + bufferInfo.byteOffset);
-    });
-  });
-
-  return Promise.all(bodyPartPromises).then(function () {
-    return fs.writeFileAsync(filename.replace(/\.gltf$/, '.glb'), glbFile);
-  });
+  return fs.writeFileAsync(filename.replace(/\.gltf$/, '.glb'), glbFile);
 }).error(function (error) {
   console.error('Failed to create binary GLTF file:');
   console.error('----------------------------------');
